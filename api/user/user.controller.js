d21a8269cd47dc69bc2a8645528e728dbd457103
oyg0420 <oyg900420@gmail.com>
2020-03-31 21:08:41 +0900

    feat(User): SignIn api
    1) 테스트 코드 작성
    2) email, password 유효성 검증 로직 작성

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -1,10 +1,46 @@
 const users = [
-  { email: "oyg0420@gmail.com", password: "123123123" },
+  { email: "oyg0420@gmail.com", password: "OOOooo123!@#" },
   { email: "oyg0421@gmail.com", password: "123123123" },
   { email: "oyg0000@gmail.com", password: "123123123" }
 ];
 
-const signIn = (req, res) => {};
+const EMAIL_REGEX = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/g;
+const PASSWORD_REGEX = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/g;
+
+const signIn = (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email.match(EMAIL_REGEX)) {
+    return res.status(400).json({
+      success: false,
+      message: "Invalid email"
+    });
+  }
+
+  if (!password.match(PASSWORD_REGEX)) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid password"
+    });
+  }
+
+  const registerdUser = users.filter(user => {
+    return user.email === email && user.password === password;
+  });
+
+  if (registerdUser.length > 0) {
+    return res.json({
+      success: true,
+      message: "Success SignIn",
+      user: registerdUser
+    });
+  }
+
+  return res.status(410).json({
+    success: false,
+    message: "User not found"
+  });
+};
 
 const signOut = (req, res) => {};
 
--- api/user/user.spec.js
+++ api/user/user.spec.js
@@ -2,6 +2,61 @@ const app = require("../../index");
 const request = require("supertest");
 const should = require("should");
 
-describe("POST /sign_in", () => {});
+describe("POST /sign_in", () => {
+  describe("성공시", () => {
+    it("success: true, user: { email, password }, message: 'Success SignIn'", done => {
+      request(app)
+        .post("/sign_in")
+        .send({ email: "oyg0420@gmail.com", password: "OOOooo123!@#" })
+        .end((err, res) => {
+          res.body.success.should.be.true();
+          res.body.should.have.property("user").which.is.an.Object();
+          res.body.message.should.be.equal("Success SignIn");
+          done();
+        });
+    });
+  });
+
+  describe("실패시", () => {
+    describe("잘못된 이메일 형식", () => {
+      it("400으로 응답", done => {
+        request(app)
+          .post("/sign_in")
+          .send({
+            email: "oyg0420",
+            password: "OOOooo123!@#"
+          })
+          .expect(400)
+          .end(done);
+      });
+    });
+
+    describe("잘못된 비밀번호 형식", () => {
+      it("401으로 응답", done => {
+        request(app)
+          .post("/sign_in")
+          .send({
+            email: "oyg0420@gmail.com",
+            password: "123123123"
+          })
+          .expect(401)
+          .end(done);
+      });
+    });
+
+    describe("가입되지 않은 유저", () => {
+      it("410으로 응답", done => {
+        request(app)
+          .post("/sign_in")
+          .send({
+            email: "oyg123@gmail.com",
+            password: "OOOooo123!@#"
+          })
+          .expect(410)
+          .end(done);
+      });
+    });
+  });
+});
 
 describe("GET /sign_out", () => {});
